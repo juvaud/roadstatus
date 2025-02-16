e444d3a54b14efcf2b97f8e4e6e4f8953eb1c772
julian <1150867+juvaud@users.noreply.github.com>
2025-02-16 14:34:24 +0100

    Spring initialized

---
+++ src/main/java/com/jv/roadstatus/RoadstatusApplication.java
@@ -0,0 +1,13 @@
+package com.jv.roadstatus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadstatusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoadstatusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jv/roadstatus/RoadstatusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jv.roadstatus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoadstatusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
